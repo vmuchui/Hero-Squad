08ca205b83b6ccde93febb2725dbdd12683a49be
vmuchui <kaigongimuchui@gmail.com>
2019-05-20 12:27:10 +0300

    added code to the empty file.

---
+++ src/main/java/VelocityTemplateEngine.java
@@ -0,0 +1,63 @@
+package spark.template.velocity;
+
+import java.io.StringWriter;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+
+import spark.ModelAndView;
+import spark.TemplateEngine;
+
+/**
+ * Template Engine based on Apache Velocity.
+ */
+public class VelocityTemplateEngine extends TemplateEngine {
+
+    private final VelocityEngine velocityEngine;
+
+    /**
+     * Constructor
+     */
+    public VelocityTemplateEngine() {
+        Properties properties = new Properties();
+        properties.setProperty("resource.loader", "class");
+        properties.setProperty(
+                "class.resource.loader.class",
+                "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+        velocityEngine = new org.apache.velocity.app.VelocityEngine(properties);
+    }
+
+    /**
+     * Constructor
+     *
+     * @param velocityEngine The velocity engine, must not be null.
+     */
+    public VelocityTemplateEngine(VelocityEngine velocityEngine) {
+        if (velocityEngine == null) {
+            throw new IllegalArgumentException("velocityEngine must not be null");
+        }
+        this.velocityEngine = velocityEngine;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String render(ModelAndView modelAndView) {
+        Template template = velocityEngine.getTemplate(modelAndView.getViewName());
+        Object model = modelAndView.getModel();
+        if (model instanceof Map) {
+            Map<?, ?> modelMap = (Map<?, ?>) model;
+            VelocityContext context = new VelocityContext(modelMap);
+            StringWriter writer = new StringWriter();
+            template.merge(context, writer);
+            return writer.toString();
+        } else {
+            throw new IllegalArgumentException("modelAndView must be of type java.util.Map");
+        }
+    }
+
+}
