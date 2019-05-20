fb1530c8354885b89c3d9b16548702898b1f6d70
vmuchui <kaigongimuchui@gmail.com>
2019-05-20 17:31:55 +0300

    added test for the method addHero that adds heroes to a squad.

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -2,7 +2,7 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class SquadTest {
-  
+
   @Test
   public void Squad_instatiatesCorrectly_true() {
     Squad wasupaa = new Squad("wasupaa",12, "fight buttsniffers");
@@ -24,5 +24,15 @@ public class SquadTest {
   public void Squad_instatiatesWithCorrectCause_String() {
     Squad wasupaa = new Squad("wasupaa",12, "fight buttsniffers");
     assertEquals("fight buttsniffers", wasupaa.getCause());
+  }
+ 
+
+  @Test
+  public void addHero_addsHeroTomHeroesArrayList_true() {
+    Squad.clear();
+    Squad wasupaa = new Squad("wasupaa",12, "fight buttsniffers");
+    Hero mnati = new Hero("name", 12, "tea", 21, "wasupaa");
+    wasupaa.addHero(mnati);
+    assertTrue(wasupaa.getHeroes().contains(mnati));
   }
 }
