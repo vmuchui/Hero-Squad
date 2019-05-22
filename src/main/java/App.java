3a8d5fd7073b2941b590764180678f1bbccdb017
vmuchui <kaigongimuchui@gmail.com>
2019-05-22 08:50:09 +0300

    added routing for forms for squad creation and creating heroes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,31 +13,91 @@ public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+    ProcessBuilder process = new ProcessBuilder();
+     Integer port;
+     if (process.environment().get("PORT") != null) {
+         port = Integer.parseInt(process.environment().get("PORT"));
+     } else {
+         port = 4567;
+     }
+
+    setPort(port);
 
     get("/", (request,response)-> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     },new VelocityTemplateEngine());
-    get("/new", (request,response)-> {
+    get("/squads/new", (request,response)-> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/success.vtl");
+      model.put("template", "templates/squad-form.vtl");
       return new ModelAndView(model, layout);
     },new VelocityTemplateEngine());
 
-    post("/new", (request,response)-> {
-     
+    
+    post("/news", (request, response)-> {
       Map<String, Object> model = new HashMap<String, Object>();
-      
+      ArrayList<Squad> squads = request.session().attribute("squads");
+      if(squads == null) {
+        squads = new ArrayList<Squad>();
+        request.session().attribute("squads", squads);
+      }
+
       String name = request.queryParams("name");
       int size = Integer.parseInt(request.queryParams("size"));
       String cause = request.queryParams("cause");
-      Squad newSquad = new Squad(name,size,cause);
-     
-      
+
+      Squad newSquad = new Squad(name, size, cause);
+      squads.add(newSquad);
+
       model.put("template", "templates/success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
-    
+    get("/squads", (request, response)-> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("squads", request.session().attribute("squads"));
+      model.put("template", "templates/squads.vtl");
+      return new ModelAndView(model,layout);
+    }, new VelocityTemplateEngine());
+    get("/news", (request, response)-> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model,layout);
+    }, new VelocityTemplateEngine());
+    get("/heroes/new", (request, response)-> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/hero-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+    get("/newhero", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/success-hero.vtl");      
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+    post("/newhero", (request, response)-> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      ArrayList<Hero> heroes = request.session().attribute("heroes");
+      if(heroes== null) {
+        heroes = new ArrayList<Hero>();
+        request.session().attribute("heroes", heroes);
+      }
+
+      String name = request.queryParams("hName");
+      int age = Integer.parseInt(request.queryParams("age"));
+      String weakness = request.queryParams("weakness");
+      String strength = request.queryParams("strength");
+      Hero newHero = new Hero(name, age, weakness, strength);
+
+      heroes.add(newHero);
+      
+      model.put("template", "templates/success-hero.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+    get("/heroes", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("heroes", request.session().attribute("heroes"));
+      model.put("template", "templates/heroes.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
