d74a19d1fd46ee7c729989306cbf7eb92f80388e
vmuchui <kaigongimuchui@gmail.com>
2019-05-22 08:54:10 +0300

    corrected tests that had heroes due to change in the class hero structure.

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -3,40 +3,40 @@ import static org.junit.Assert.*;
 
 public class HeroTest {
 
-  
+
   @Test
   public void newHero_instatiatesCorrectly_boolean() {
-    Hero superMario = new Hero("Super Mario",102, "Garlic", 12, "NduthiGeng");
+    Hero superMario = new Hero("Super Mario",102, "Garlic", "superspeed");
     assertEquals(true, superMario instanceof Hero);
   }
 
   @Test
   public void Hero_instatiatesCorrectlyWithName_String() {
-    Hero superMario = new Hero("Super Mario", 102, "Garlic", 10, "Kimonyoski");
+    Hero superMario = new Hero("Super Mario", 102, "Garlic", "superstrength");
     assertEquals("Super Mario", superMario.getName());
   }
 
   @Test
   public void Hero_instatiatesCorrectlyWithAge_int() {
-    Hero superMario = new Hero("SuperMario", 102, "Garlic", 12, "Mbogi");
+    Hero superMario = new Hero("SuperMario", 102, "Garlic", "superspeed");
     assertEquals(102, superMario.getAge());
   }
 
   @Test
   public void Hero_instatiatesCorrectlyWithWeakness_String() {
-    Hero superMario = new Hero("Super Mario", 102, "Garlic", 10, "mambati");
+    Hero superMario = new Hero("Super Mario", 102, "Garlic", "superspeed");
     assertEquals("Garlic", superMario.getWeakness());
   }
 
   @Test
   public void Hero_returnsCorrectSpecialPowers_String() {
-    Hero superMario = new Hero("Super Mario", 102, "Garlic", 12, "Watiaji");
-    assertEquals(12, superMario.getStrength());
+    Hero superMario = new Hero("Super Mario", 102, "Garlic", "superspeed");
+    assertEquals("superspeed", superMario.getStrength());
   }
-
   @Test
-  public void Hero_canBelongToSquad_String() {
-    Hero superMario = new Hero("super", 23, "onions", 12, "SuperSoupes");
-    assertEquals("SuperSoupes", superMario.getSquad());
+  public void find_returnsHero_Hero() {
+    Hero muchui = new Hero("name", 21, "shorties", "tech-guy");
+    assertEquals(muchui, Hero.find(1));
   }
+  
 }
